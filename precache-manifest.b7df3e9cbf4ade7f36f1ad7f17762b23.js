3f4f069da07e0da8d3837260b2710c2584b7f774
Eric Lin <c84lin@uwaterloo.ca>
2020-05-13 16:20:52 -0400

    Updates

---
+++ precache-manifest.b7df3e9cbf4ade7f36f1ad7f17762b23.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd653dc348594ab2120220bd3f9415f0",
+    "url": "/badchess/index.html"
+  },
+  {
+    "revision": "6f854bca4c81925a5902",
+    "url": "/badchess/static/css/main.5db9d49f.chunk.css"
+  },
+  {
+    "revision": "b83e38d1bf0933466ea1",
+    "url": "/badchess/static/js/2.a828f657.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/badchess/static/js/2.a828f657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f854bca4c81925a5902",
+    "url": "/badchess/static/js/main.4d627cb3.chunk.js"
+  },
+  {
+    "revision": "51b4104b1b33fae9ea3a",
+    "url": "/badchess/static/js/runtime-main.12981bec.js"
+  },
+  {
+    "revision": "9b27dd1e629bfd7e52925590b1675331",
+    "url": "/badchess/static/media/black_horse.9b27dd1e.png"
+  },
+  {
+    "revision": "75f0836cb86649d606d6fa557bada7e6",
+    "url": "/badchess/static/media/black_king.75f0836c.png"
+  },
+  {
+    "revision": "5b2800831eb75e69602294f19e2f06f3",
+    "url": "/badchess/static/media/black_queen.5b280083.png"
+  },
+  {
+    "revision": "858bcbcc44fad0ccd1e3334d7b5843b1",
+    "url": "/badchess/static/media/white_bishop.858bcbcc.png"
+  },
+  {
+    "revision": "fb87627afb6e86125ad61657e2315547",
+    "url": "/badchess/static/media/white_horse.fb87627a.png"
+  },
+  {
+    "revision": "3ff359d6c5bf391d2d6ca1fe456f7e44",
+    "url": "/badchess/static/media/white_king.3ff359d6.png"
+  },
+  {
+    "revision": "7cf685a703216a34b8cf02ef621f6ad5",
+    "url": "/badchess/static/media/white_queen.7cf685a7.png"
+  }
+]);
